5ec33829eabada90d07e57c770fe190d70ae03f6
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2023-12-04 22:35:25 +0100

    random enemies, no collide

---
+++ js/enemy.js
@@ -0,0 +1,29 @@
+class Enemy {
+    constructor(gameScreen) {
+        this.gameScreen = gameScreen;
+        this.left = Math.floor(Math.random() * 400 + 50);
+        this.top = -80;
+        this.width = 46;
+        this.height = 36;
+        this.element = document.createElement("img");
+        this.element.src = "./images/enemy0.png";
+        this.element.style.position = "absolute";
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+        this.gameScreen.appendChild(this.element);
+
+    }
+    updatePosition() {
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+    }
+    move() {
+        console.log("enemy.move")
+
+      this.top += 3;
+
+      this.updatePosition();
+    }
+}
--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ class Game {
         this.player = null;
         this.height = 600;
         this.width = 500;
-        this.obstacles = [];
+        this.enemies = [];
         this.score = 0;
         this.lives = 3;
         this.gameIsOver = false;
@@ -18,7 +18,8 @@ class Game {
             59,
             43,
             "./images/plane.png"
-        )
+        );
+        this.enemy = new Enemy(this.gameScreen)
     }
 
     start(){
@@ -40,5 +41,23 @@ class Game {
     update(){
         this.player.move();
         this.player.updateBullets();
+        
+
+        for(let i=0; i< this.enemies.length; i++){
+            const enemy = this.enemies[i]
+            enemy.move();
+
+            if (enemy.top > this.height + 50) {
+                enemy.element.remove();
+                this.enemies.splice(i, 1);
+                i--; // Adjust index after removing an element
+            }
+        }
+        
+    
+
+        if (Math.random() > 0.98 && this.enemies.length < 5) {
+            this.enemies.push(new Enemy(this.gameScreen));
+        }
     }
 }
